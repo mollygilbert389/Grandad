c4c57b615d6f2b16dccf22f756ce023bd00b8db8
Molly Gilbert <msgilbert389@gmail.com>
2019-06-19 15:30:56 -0500

    adding initial files

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+$("#gifSpace").hide();
+
+
+$("#gifs").on("click", function(){
+    gifNum = 10;
+    $("#gifSpace").show();
+    $("#gifSpace").empty()
+    var x = $(this).data("search");
+    console.log(x)
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + x +
+    "&api_key=zG9cWB2fdhAqDcr9UfzJFW6DLBO8hMXP&limit=" + gifNum;
+
+    $.ajax({url:queryURL,method:'GET'})
+        .done(function(response){
+            for (var i=0;i<response.data.length;i++){
+            $("#gifSpace").append("<img src='"+response.data[i].images.downsized.url+"'>")
+            }
+        })
+})
+
+})
